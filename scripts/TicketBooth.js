481286ec3162712d121f9d1c3a6019729c7a7bd1
sorachung <sorachung.src@gmail.com>
2021-12-09 12:12:57 -0600

    added five buttons for buying tickets

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -6,7 +6,19 @@ eventHub.addEventListener()
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
-            Add button here
+            <button id="rideTicket">Ride Ticket</button>
+        </div>
+        <div class="ticketBooth">
+            <button id="foodTicket">Food Ticket</button>
+        </div>
+        <div class="ticketBooth">
+            <button id="gameTicket">Game Ticket</button>
+        </div>
+        <div class="ticketBooth">
+            <button id="sideshowTicket">Sideshow Ticket</button>
+        </div>
+        <div class="ticketBooth">
+            <button id="fullPackageTicket">Full Package Ticket</button>
         </div>
     `
 }
