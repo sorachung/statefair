61dd31060a12fe58d0800ac5814773271fa072f4
sorachung <sorachung.src@gmail.com>
2021-12-09 12:16:56 -0600

    made clicking ride ticket button do something

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -2,7 +2,12 @@ const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+    eventHub.addEventListener("rideTicketPurchased", customEvent => {
+        TotalTicket();
+        contentTarget.innerHTML += `
+            <div class="rider person">
+            </div>
+            `
     })
 }
 
